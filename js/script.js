447ee53a5d1f336f9e5e848c968cbafd260b4e03
nurali-code <nur3.dav.97@gmail.com>
2024-03-04 12:27:32 +0500

    all done

--- js/script.js
+++ js/script.js
@@ -1,18 +1,17 @@
 $('#send_form').on('submit', function (e) {
     e.preventDefault();
+    $('#submit').attr('disabled', 'true');
     var formData = $(this).serialize();
-    var googleAppsScriptUrl = 'https://script.google.com/macros/s/AKfycbwAjIYCrK5WqgYLeBwMxhR0ba-dnLv61nxcFfByIb22jvTdwbKGmanLahSfqUEOemgD/exec';
-
-    console.log(formData);
+    var googleAppsScriptUrl = 'https://script.google.com/macros/s/AKfycbw4jYnbkx8OYYK6Ww37zEZxSRZBQOtqlxri8rd6U7itl2rXri9yIFAhJ784PazbjzEg/exec';
 
     $.ajax({
         type: 'POST',
         url: googleAppsScriptUrl,
         data: formData,
         success: function (response) {
-            $(formParet).find('.btn').removeAttr('disabled');
-            nextTab(formParet);
-            console.log(response);
+            alert('Application sent!')
+            $('#send_form')[0].reset();
+            $('#submit').removeAttr('disabled');
         },
         error: function (error) { console.error('Произошла ошибка:', error); }
     });
